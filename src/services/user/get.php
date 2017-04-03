76d0246daf0ca5bce9b2388edc0b7af0657d5f16
Kitro <Kitro@192.168.0.11>
2017-04-03 22:43:59 +0200

    add: tagFactory add: Factory abstrakte klasse add: Tag-Get Dienst

--- src/services/user/get.php
+++ src/services/user/get.php
@@ -1,14 +1,15 @@
 <?php
+
 error_reporting(E_ALL);
-ini_set("display_errors", 1); 
+ini_set("display_errors", 1);
 
 require_once("./../../factories/userFactory.php");
 
 if (isset($_GET["Id"]))
-{	
-	$id = intval($_GET["Id"]);		
-	$userFactory = new UserFactory();		
-	echo json_encode($userFactory->ConvertToAssocArray($userFactory->Get($id)));
+{
+    $id = intval($_GET["Id"]);
+    $userFactory = new UserFactory();
+    echo json_encode($userFactory->ConvertToAssocArray($userFactory->Get($id)));
 }
 
 echo "";
