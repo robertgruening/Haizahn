4e49dd17579b9ec535256ea6c5e9cf869ceae827
TheKitro <kitro@gmx.de>
2018-06-04 21:11:14 +0200

    PHP-UnitTest hinzugefügt

---
+++ tst/HelloTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use  PHPUnit\Framework\TestCase;
+
+//require_once(__DIR__."//)
+
+final class Test extends TestCase
+{
+public function testHelloTest()
+{
+    $this->assertTrue(true);
+}
+
+}
